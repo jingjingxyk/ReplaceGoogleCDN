b67ef6fc8c4e9728f949107c9d13127115033a5c
jingjingxyk <zonghengbaihe521@qq.com>
2025-03-05 12:30:53 +0800

    format code

--- tools/format-code.sh
+++ tools/format-code.sh
@@ -12,8 +12,12 @@ __PROJECT__=$(readlink -f ${__DIR__}/../)
 
 cd ${__PROJECT__}
 
+export PATH=${__PROJECT__}/bin/runtime/node/bin:$PATH
+
 if [[ ! -f ${__PROJECT__}/bin/runtime/node/bin/node ]]; then
   bash tools/setup-nodejs-runtime.sh --mirror china
+  npm install --registry=https://registry.npmmirror.com
+  chmod a+x ${__PROJECT__}/node_modules/.bin/prettier
 fi
 
 npx prettier --write .
